4ca00fa7a62a749829915469eb8454384ab466cd
Matias Ruiz <matiasruiz94m@gmail.com>
2022-08-24 12:12:12 -0300

    ej 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -9,8 +9,14 @@ def maximo_basico(a: float, b: float) -> float:
         - No utilizar ELSE
         - No utilizar la función max
     """
+#inicio
+   
+    if a > b:
+        return float(a)
+    return float(b)
+    
 
-
+#fin
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
 assert maximo_basico(9, 18) == 18
@@ -24,7 +30,7 @@ def maximo_libreria(a: float, b: float) -> float:
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
-
+    return float (max(a,b))
 
 # NO MODIFICAR - INICIO
 assert maximo_libreria(10, 5) == 10
@@ -40,7 +46,7 @@ def maximo_ternario(a: float, b: float) -> float:
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
     """
 
-
+    return a if a>b else b 
 # NO MODIFICAR - INICIO
 assert maximo_ternario(10, 5) == 10
 assert maximo_ternario(9, 18) == 18
